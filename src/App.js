2edc051cd481b513191dbee94749b9874a82022f
Nataliia333 <n.o.prykhodko@gmail.com>
2021-03-04 20:52:08 +0200

    task 1 Profile без стилей

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Profile from './components/Profile/Profile';
 
-function App() {
+import user from './components/Profile/user.json';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
