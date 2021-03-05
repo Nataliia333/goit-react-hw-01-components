fbf1e617528a3e12c6e03cc8a37b7d9f6f9446c8
Nataliia333 <n.o.prykhodko@gmail.com>
2021-03-05 16:40:21 +0200

    task-3 styles

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import defaultImage from './default.jpg';
+import styles from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const classStatus = [styles.status, styles.isOnline];
   return (
-    <li className="item">
-      <span className="status">{isOnline}</span>
-      <img className="avatar" src={avatar} alt={name} width="48" />
-      <p className="name">{name}</p>
+    <li className={styles.item}>
+      <span className={isOnline ? classStatus.join(' ') : styles.status}></span>
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
     </li>
   );
 };
